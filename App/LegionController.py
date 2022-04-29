461f3dc4520c403e08fa5317793e8bd1694492b0
NUTSU7 <BivolLiviu62@gmail.com>
2022-04-29 23:05:15 +0300

    Button activation depending on the current powerMode
    Signed-off-by: NUTSU7 <BivolLiviu62@gmail.com>

--- App/LegionController.py
+++ App/LegionController.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+from tempfile import tempdir
 from tkinter import*
 from PIL import ImageTk, Image
 import os
@@ -9,22 +11,36 @@ root.resizable(False, False)
 
 #Vars
 currentMode = 0
-
-
+temp = 0
 def fcurrentMode():
+    global temp
+    global currentMode
     f = open("/sys/kernel/LegionController/powerMode", "r")
     temp=f.read()
+    f.close()
+    label2['text']=temp
     if temp=='performance': 
         currentMode=1
         perfBtn.configure(bg='#2333B4', activebackground='#2333B4')
+        balancedBtn.configure(bg='#676871', activebackground='#676871')
+        quietBtn.configure(bg='#676871', activebackground='#676871')
+        saveBtn.configure(bg='#676871', activebackground='#676871')
+        balancedBtn.configure(bg='#676871', activebackground='#676871')
     elif temp=='balanced': 
         currentMode=2
+        perfBtn.configure(bg='#676871', activebackground='#676871')
         balancedBtn.configure(bg='#2333B4', activebackground='#2333B4')
+        quietBtn.configure(bg='#676871', activebackground='#676871')
+        saveBtn.configure(bg='#676871', activebackground='#676871')
+        balancedBtn.configure(bg='#676871', activebackground='#676871')
     elif temp=='quiet': 
         currentMode=3
+        perfBtn.configure(bg='#676871', activebackground='#676871')
+        balancedBtn.configure(bg='#676871', activebackground='#676871')
         quietBtn.configure(bg='#2333B4', activebackground='#2333B4')
-    
-
+        saveBtn.configure(bg='#676871', activebackground='#676871')
+        balancedBtn.configure(bg='#676871', activebackground='#676871')
+    label1['text']='1'
 
 #Images
 #Window icon
@@ -62,23 +78,27 @@ modes.place(rely=0.80, relheight=0.20, relwidth=1)
 
 
 #Page
-
+label1 = Label(page)
+label1.place(anchor=CENTER,relx=0.25, rely=0.25, relheight=0.2, relwidth=0.5)
+label2 = Label(page)
+label2.place(anchor=CENTER,relx=0.25, rely=0.5, relheight=0.2, relwidth=0.5)
 
 # Buttons
-perfBtn = Button(modes, image=perfIcon, bg='#676871', activebackground='#676871')
+perfBtn = Button(modes, image=perfIcon)
 perfBtn.place(relwidth=0.20, relheight=1)
 
-balancedBtn = Button(modes, image=balancedIcon, bg='#676871', activebackground='#676871')
+balancedBtn = Button(modes, image=balancedIcon)
 balancedBtn.place(relx=0.20, relwidth=0.20, relheight=1)
 
-quietBtn = Button(modes,image=quietIcon, bg='#676871', activebackground='#676871')
+quietBtn = Button(modes,image=quietIcon)
 quietBtn.place(relx=0.40, relwidth=0.20, relheight=1)
 
-saveBtn = Button(modes, image=saveIcon, bg='#676871', activebackground='#676871')
+saveBtn = Button(modes, image=saveIcon)
 saveBtn.place(relx=0.60, relwidth=0.20, relheight=1)
 
-settingsBtn = Button(modes, image=settingsIcon, bg='#676871', activebackground='#676871')
+settingsBtn = Button(modes, image=settingsIcon)
 settingsBtn.place(relx=0.80, relwidth=0.20, relheight=1)
 
-root.after_idle(fcurrentMode())
+fcurrentMode()
+
 root.mainloop()
