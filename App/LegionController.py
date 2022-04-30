81df50a7e2d5a01c04730b9044ee0f4e9469932f
NUTSU7 <BivolLiviu62@gmail.com>
2022-04-30 19:50:25 +0300

    Early form of threading for lighting up the buttons
    Signed-off-by: NUTSU7 <BivolLiviu62@gmail.com>

--- App/LegionController.py
+++ App/LegionController.py
@@ -2,9 +2,7 @@
 from tempfile import tempdir
 from tkinter import *
 from PIL import ImageTk, Image
-import os
-import threading
-import time
+import os, threading, time
 
 root = Tk()
 root.geometry('500x500')
@@ -13,9 +11,11 @@ root.resizable(False, False)
 
 #Vars
 currentMode = 0
-temp = 0
+saveBtnPressedValue = False
+
+#Functions
 def fcurrentMode():
-    global temp
+    temp = 0
     global currentMode
     f = open("/sys/kernel/LegionController/powerMode", "rt")
     temp=f.read()
@@ -26,25 +26,34 @@ def fcurrentMode():
         perfBtn.configure(bg='#2333B4', activebackground='#2333B4')
         balancedBtn.configure(bg='#676871', activebackground='#676871')
         quietBtn.configure(bg='#676871', activebackground='#676871')
-        saveBtn.configure(bg='#676871', activebackground='#676871')
-        settingsBtn.configure(bg='#676871', activebackground='#676871')
-        settingsBtn.after(1000,fcurrentMode)
+        quietBtn.after(1000,fcurrentMode)
     elif temp=='balanced': 
         currentMode=2
         perfBtn.configure(bg='#676871', activebackground='#676871')
         balancedBtn.configure(bg='#2333B4', activebackground='#2333B4')
         quietBtn.configure(bg='#676871', activebackground='#676871')
-        saveBtn.configure(bg='#676871', activebackground='#676871')
-        settingsBtn.configure(bg='#676871', activebackground='#676871')
-        settingsBtn.after(1000,fcurrentMode)
+        quietBtn.after(1000,fcurrentMode)
     elif temp=='quiet': 
         currentMode=3
         perfBtn.configure(bg='#676871', activebackground='#676871')
         balancedBtn.configure(bg='#676871', activebackground='#676871')
         quietBtn.configure(bg='#2333B4', activebackground='#2333B4')
-        saveBtn.configure(bg='#676871', activebackground='#676871')
-        settingsBtn.configure(bg='#676871', activebackground='#676871')
-        settingsBtn.after(1000,fcurrentMode)
+        quietBtn.after(1000,fcurrentMode)
+
+def saveBtnPressed():
+    global saveBtnPressedValue
+    saveBtnPressedValue = True
+
+def btnLightUp():
+    while True:
+        global saveBtnPressedValue
+        if saveBtnPressedValue == True:
+            saveBtn.configure(bg='#2333B4', activebackground='#2333B4')
+            time.sleep(0.35)
+            saveBtn.configure(bg='#676871', activebackground='#676871')
+            saveBtnPressedValue = False
+        time.sleep(0.2)
+
 
 #Images
 #Window icon
@@ -96,13 +105,16 @@ balancedBtn.place(relx=0.20, relwidth=0.20, relheight=1)
 quietBtn = Button(modes,image=quietIcon)
 quietBtn.place(relx=0.40, relwidth=0.20, relheight=1)
 
-saveBtn = Button(modes, image=saveIcon)
+saveBtn = Button(modes, image=saveIcon,bg='#676871', activebackground='#676871', command=saveBtnPressed)
 saveBtn.place(relx=0.60, relwidth=0.20, relheight=1)
 
-settingsBtn = Button(modes, image=settingsIcon)
+settingsBtn = Button(modes, image=settingsIcon,bg='#676871', activebackground='#676871')
 settingsBtn.place(relx=0.80, relwidth=0.20, relheight=1)
 
-#threading.Thread(target=fcurrentMode).start()
+saveBtn.after(200, lambda: saveBtn.configure(bg='#2333B4', activebackground='#2333B4'))
+
+#saveBtnTh = threading.Thread(target=btnLightUp).start()
+
 fcurrentMode()
 
 root.mainloop()
